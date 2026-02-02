2c8690d1f4d6ab72a2a72d72d845155f435ca7bc
さとう <nimu3103@gmail.com>
2026-02-02 06:30:31 +0000

    test: ルーティング変更に合わせてテストコードを修正

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    # home_index_url を root_url に書き換えます
+    get root_url
     assert_response :success
   end
 end
