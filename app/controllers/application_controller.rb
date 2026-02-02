5f74198deccbced7d4b9329de5cfca8e3d898e03
さとう <nimu3103@gmail.com>
2026-02-02 06:08:53 +0000

    feat: ユーザー識別用のUserモデル作成とcookiesによる自動生成ロジックの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    # uuid を session_token に書き換え
+    token = cookies.permanent[:user_uuid]
+
+    @current_user = User.find_or_create_by!(session_token: token) do |user|
+      user.session_token = SecureRandom.uuid
+    end
+
+    cookies.permanent[:user_uuid] = @current_user.session_token
+  end
 end
--- db/migrate/20260202020750_create_users.rb
+++ db/migrate/20260202020750_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :session_token, null: false
+      # uuid カラムを追加し、空っぽ（null）を禁止にする
+      t.string :uuid, null: false
+
       t.timestamps
     end
-    add_index :users, :session_token, unique: true
+    # uuid で検索を速くし、かつ重複（同じID）を絶対に許さない設定
+    add_index :users, :uuid, unique: true
   end
 end
