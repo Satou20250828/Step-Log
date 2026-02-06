5edb05fa295dc0ab77a07ce195e5f77a2ce09114
さとう <nimu3103@gmail.com>
2026-02-06 14:14:41 +0000

    Make habits migration idempotent

--- db/migrate/20260202115410_create_habits.rb
+++ db/migrate/20260202115410_create_habits.rb
@@ -1,6 +1,6 @@
 class CreateHabits < ActiveRecord::Migration[7.1]
   def change
-    create_table :habits do |t|
+    create_table :habits, if_not_exists: true do |t|
       # index: { unique: true } を追加して「1人1つ」をDBレベルで強制
       t.references :user, null: false, foreign_key: true, index: { unique: true }
       t.string :name, null: false
